30ccfcd05fe11eded5a49fccbf1757af07f249cb
Wang-Guangyu <wangguangyu271828@gmail.com>
2022-08-15 15:00:15 +0800

    first

--- basic_content/const/const_num.cpp
+++ basic_content/const/const_num.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main(){
     const int b = 10;
-    b = 0; //error
+/*    b = 0; //error */
     const string s = "helloworld";
-    const int i,j=0;
+    const int i=0, j=0;
+    std::cout << s << std::endl;
+
+    return 0;
 }
